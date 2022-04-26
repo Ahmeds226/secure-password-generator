dadec662a12c4b36a4a80113455c319839ba7580
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-04-26 17:43:58 +0100

    Completed JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,133 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+
+//User story:
+//User selects password length:
+function getPasswordLength() {
+  const userInput = prompt(
+    "Please choose a length between 8 and 128 characters",
+    "10"
+  );
+  if ((userInput) => 8 && userInput <= 128) {
+    return userInput;
+  } else {
+    alert("Due to an invalid unput we have set the value to 10.");
+    return 10;
+  }
+}
+
+//User selects whether they want numbers or not:
+function CheckNumbers() {
+  if (
+    confirm("Would you like to include numbers in your new password?") == true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//User selects whether they want special characters or not:
+function CheckSpecialCharacters() {
+  if (
+    confirm(
+      "Would you like to include special characters such as !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~ in your new password?"
+    ) == true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//User selects whether they want lowercase letters  or not:
+function CheckLowercaseLetters() {
+  if (
+    confirm(
+      "Would you like to include lowercase letters in your new password?"
+    ) == true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//User selects whether they want uppercase or not:
+function CheckUppercaseLetters() {
+  if (
+    confirm(
+      "Would you like to include uppercase letters in your new password?"
+    ) == true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Checks the user has selected atleast one option:
+if (
+  CheckNumbers == false &&
+  CheckSpecialCharacters == false &&
+  CheckLowercaseLetters == false &&
+  CheckUppercaseLetters == false
+) {
+  alert("You must inculde one type of data to include in your password.");
+}
+
+//Function below gathers the users desired criteria and confines it into a single string:
+function addUserCriteria() {
+  const addNumbers = CheckNumbers();
+  const UsersCharacters = "";
+  if (addNumbers === true) {
+    const passwordNumbers = "1234567890";
+    UsersCharacters = UsersCharacters.concat(passwordNumbers);
+  }
+  const addSpecialCharacters = CheckSpecialCharacters();
+  if (addSpecialCharacters === true) {
+    const passwordSpecialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    // \ Escape characters to treat special characters as just string.
+    UsersCharacters = UsersCharacters.concat(passwordSpecialCharacters);
+  }
+  const addLowercaseLetters = CheckLowercaseLetters();
+  if (addLowercaseLetters === true) {
+    const passwordLowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    UsersCharacters = UsersCharacters.concat(passwordLowercaseLetters);
+  }
+  const addUppercaseLetters = CheckUppercaseLetters();
+  if (addUppercaseLetters === true) {
+    const passwordUppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    UsersCharacters = UsersCharacters.concat(passwordUppercaseLetters);
+  }
+  return UsersCharacters;
+}
 
 // main function to generate the password
-var generatePassword = () => {};
-// need to validate this convert string to integer
+// Generate new password with the users needs alongisder their desired length:
+function generatePassword = (){
+  const UsersCharacters = addUserCriteria();
+  const NewPasswordLength = getPasswordLength
+  const NeedsandLength = UsersCharacters.length
+
+  const password = "";
+  for (const passwordlength = 0; passwordlength < NewPasswordLength; passwordlength++) {
+    const randomNumber = Math.random() * NeedsandLength;
+    const randomChoice = Math.floor(randomNumber);
+    const newCharacters = UsersCharacters.substring(randomChoice, randomChoice + 1);
+    password = password.concat(newCharacters);
+  }
+  return password;
+
+};
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// // Functions:
-// 1.getpasswordlength:
-// get string
-// convert to integer
-// check rule
-// If ok produce number
-
-// \\ Converts string to number
-// \\ Need an if statement
-// \\ Create an alert
-// \\ If rule is true produce number
-// \\ if false then produce another alert
-
-// 2.getpasswordcriteria:
-// Lowercase,uppercase,number and characters
-// Return generates an array of all the criteria they wanted for example yes to lc then an array of lc leters shoudl be generated
-// If for all criteria if true push array if no then move to next box
-// check length if it doesnt meet the rule then alert
-
-// 3.generatenewpassword
-// IN password length and password criteria
-// OUT new password as a string
-
-// // Array:
-// 1. PasswordArray
-// criteria must be pushed into an array
